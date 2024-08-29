33d9fad73917b231623c5bd211e198371b52ccd2
Jose Ocampo <ocampojose305@gmail.com>
2024-08-29 16:46:22 -0500

    added flexible english and spanish buttons to index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,11 @@
-import { Text, View } from "react-native";
+import { Text, View, Button, Alert, StyleSheet } from "react-native";
 
+const styles = StyleSheet.create({
+    buttonStyle: {
+        margin: 10,
+        width: 100,
+    },
+});
 export default function Index() {
   return (
     <View
@@ -9,7 +15,23 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Welcome to DiaBuddy.</Text>
+        <Text>Please select your language</Text>
+        <View style={{ flexDirection:"row" }}>
+            <View style={styles.buttonStyle}>
+                <Button
+                    title="English"
+                    onPress={() => Alert.alert('English Selected')}
+                />
+            </View>
+            <View style={styles.buttonStyle}>
+                <Button
+                    title="Spanish"
+                    onPress={() => Alert.alert('Espanol Escogido')}
+                />
+            </View>
+        </View>
+
     </View>
   );
 }
