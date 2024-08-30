cf7db750f7a820732fc19d8590c663c64eb875da
Jose Ocampo <ocampojose305@gmail.com>
2024-08-30 17:15:50 -0500

    added hero1 png to home and added colors to buttons

--- app/index.tsx
+++ app/index.tsx
@@ -1,37 +1,48 @@
-import { Text, View, Button, Alert, StyleSheet } from "react-native";
+import { Text, View, TouchableOpacity, Alert, StyleSheet, Image } from "react-native";
 
 const styles = StyleSheet.create({
     buttonStyle: {
         margin: 10,
         width: 100,
+        backgroundColor: '#DC7633',
+        padding: 10,
+        alignItems: 'center',
+        borderRadius: 5,
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 16,
     },
 });
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Welcome to DiaBuddy.</Text>
-        <Text>Please select your language</Text>
-        <View style={{ flexDirection:"row" }}>
-            <View style={styles.buttonStyle}>
-                <Button
-                    title="English"
-                    onPress={() => Alert.alert('English Selected')}
-                />
-            </View>
-            <View style={styles.buttonStyle}>
-                <Button
-                    title="Spanish"
-                    onPress={() => Alert.alert('Espanol Escogido')}
-                />
-            </View>
-        </View>
 
-    </View>
-  );
+export default function Index() {
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <Text>Please select your language</Text>
+            <View style={{ flexDirection: "row" }}>
+                <TouchableOpacity
+                    style={styles.buttonStyle}
+                    onPress={() => Alert.alert("English Selected")}
+                >
+                    <Text style={styles.buttonText}>English</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.buttonStyle}
+                    onPress={() => Alert.alert("Espanol Escogido")}
+                >
+                    <Text style={styles.buttonText}>Spanish</Text>
+                </TouchableOpacity>
+            </View>
+
+            <Image
+                source={require('../assets/images/DiaBuddyHeros/Hero1.png')}
+            />
+        </View>
+    );
 }
