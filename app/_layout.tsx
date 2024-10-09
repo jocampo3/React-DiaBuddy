b037b45ec23eeabd370367ae3bbacf43ec223802
Brian Toone <brtoone@samford.edu>
2024-10-09 10:20:38 -0500

    header options on stack.screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,14 +5,21 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-        <Stack.Screen name="index" />
-        <Stack.Screen name="pages/HomePage"/>
-        <Stack.Screen name="pages/SelectLangPage"/>
-        <Stack.Screen name="pages/about/AboutPage" />
-        <Stack.Screen name="pages/checkinsulin/CheckInsulinPage"/>
-        <Stack.Screen name="pages/checkglucose/CheckGlucosePage"/>
-        <Stack.Screen name="pages/recordglucose/RecordGlucosePage"/>
-        <Stack.Screen name="pages/food/FoodPage"/>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="pages/HomePage" options={{ headerShown: true, title:"Why am i a superhero?", headerStyle: {
+            backgroundColor: 'yellow', // Background color of the header
+          },
+          headerTitleStyle: {
+            fontWeight: 'bold', // Font weight of the title
+            color: 'black', // Color of the title text
+          },
+          headerTintColor: '#fff', }} />
+      <Stack.Screen name="pages/SelectLangPage" options={{ headerShown: false }} />
+      <Stack.Screen name="pages/about/AboutPage" options={{ headerShown: false }} />
+      <Stack.Screen name="pages/checkinsulin/CheckInsulinPage" options={{ headerShown: false }} />
+      <Stack.Screen name="pages/checkglucose/CheckGlucosePage" options={{ headerShown: false }} />
+      <Stack.Screen name="pages/recordglucose/RecordGlucosePage" options={{ headerShown: false }} />
+      <Stack.Screen name="pages/food/FoodPage" options={{ headerShown: false }} />
     </Stack>
   );
 }
