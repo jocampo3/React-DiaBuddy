384209a0a13af84fe6b8d94c73e2c3c5ff4da7c0
Grant King <gking@Grants-MacBook-Pro-2.local>
2024-11-08 15:05:58 -0600

    cont dev on sqlite

--- app/database.ts
+++ app/database.ts
@@ -2,19 +2,36 @@ import * as SQLite from 'expo-sqlite';
 
 const db = await SQLite.openDatabaseAsync('diabuddy');
 
-export const setupDatabase = (): void => {
-    db.execAsync(
-        `CREATE TABLE IF NOT EXISTS glucoselog (
-         id INTEGER PRIMARY KEY AUTOINCREMENT,
-         datereading DATE,
-         breakfast REAL,
-         lunch REAL,
-         dinner REAL,
-         bedtime REAL,
-         notes TEXT,
-         created DATETIME DEFAULT CURRENT_TIMESTAMP,
-         modified DATETIME DEFAULT CURRENT_TIMESTAMP
-       )`
+export const setupDatabase = async (): Promise<void> => {
+    await db.execAsync(
+        `CREATE TABLE IF NOT EXISTS glucoselog
+         (
+             id
+             INTEGER
+             PRIMARY
+             KEY
+             AUTOINCREMENT,
+             datereading
+             DATE,
+             breakfast
+             REAL,
+             lunch
+             REAL,
+             dinner
+             REAL,
+             bedtime
+             REAL,
+             notes
+             TEXT,
+             created
+             DATETIME
+             DEFAULT
+             CURRENT_TIMESTAMP,
+             modified
+             DATETIME
+             DEFAULT
+             CURRENT_TIMESTAMP
+         )`
     );
 };
 
