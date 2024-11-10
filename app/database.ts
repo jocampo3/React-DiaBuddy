0bf128784bab60e239e60818ff00402ada29f305
Grant King <gking@Grants-MacBook-Pro-2.local>
2024-11-10 17:03:27 -0600

    i fixed issue: not compiling, new issue: cannot find native module 'ExpoSQLiteNext'

--- app/database.ts
+++ app/database.ts
@@ -1,36 +1,20 @@
 import * as SQLite from 'expo-sqlite';
 
-const db = await SQLite.openDatabaseAsync('diabuddy');
+let db: any = null;
+
+const initializeDatabase = async () => {
+    db = await SQLite.openDatabaseAsync('diabuddy');
+};
 
 export const setupDatabase = async (): Promise<void> => {
+    if (!db) await initializeDatabase();
+
     await db.execAsync(
-        `CREATE TABLE IF NOT EXISTS glucoselog
+        `CREATE TABLE IF NOT EXISTS glucoselog;
          (
-             id
-             INTEGER
-             PRIMARY
-             KEY
-             AUTOINCREMENT,
-             datereading
-             DATE,
-             breakfast
-             REAL,
-             lunch
-             REAL,
-             dinner
-             REAL,
-             bedtime
-             REAL,
-             notes
-             TEXT,
-             created
-             DATETIME
-             DEFAULT
-             CURRENT_TIMESTAMP,
-             modified
-             DATETIME
-             DEFAULT
-             CURRENT_TIMESTAMP
+             id INTEGER PRIMARY KEY AUTOINCREMENT, datereading DATE, breakfast REAL, lunch REAL,
+             dinner REAL, bedtime REAL, notes TEXT, created DATETIME DEFAULT CURRENT_TIMESTAMP,
+             modified DATETIME DEFAULT CURRENT_TIMESTAMP
          )`
     );
 };
